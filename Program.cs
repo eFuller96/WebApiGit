f737be7634588f37c9b764ccfc23fd32557e1294
Ellie <eleanor.fuller@red-gate.com>
2020-05-16 20:48:00 +0100

    WIP: get first commit

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Net.Http;
+using System.Net.NetworkInformation;
 using System.Text.Json;
 using System.Threading.Tasks;
 using WebApiGit;
@@ -10,7 +11,8 @@ namespace webapi
     {
         public static async Task Main(string[] args)
         {
-            await GetGithubAccount();
+            //await GetGithubAccount();
+            await GetFirstCommit();
         }
 
         private static async Task GetGithubAccount()
@@ -30,5 +32,22 @@ namespace webapi
 
             Console.Write($"Name: {gitAccountDetails.Name}, Login: {gitAccountDetails.Login}");
         }
+
+        private static async Task GetFirstCommit()
+        {
+            using var httpClient = new HttpClient();
+            using var request = new HttpRequestMessage(new HttpMethod("GET"), "https://api.github.com/search/commits?q=author:eFuller96+repo:red-gate/foundry-labs&sort=author-date&order=desc");
+            request.Headers.TryAddWithoutValidation("Authorization", "token adbde95cd6986e8e0875b2ceb8a08f23b557a7fd");
+            request.Headers.TryAddWithoutValidation("Accept", "application/vnd.github.cloak-preview");
+            request.Headers.TryAddWithoutValidation("User-Agent", "agent");
+            
+            var response = await httpClient.SendAsync(request);
+            var content = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(content);
+        }
     }
 }
+
+//curl -H "Accept: application/vnd.github.cloak-preview" -H "Authorization: token adbde95cd6986e8e0875b2ceb8a08f23b557a7fd" https://api.github.com/repos/red-gate/foundry-labs/commits\?author\=eFuller96
+//curl -H "Accept: application/vnd.github.cloak-preview" -H "Authorization: token adbde95cd6986e8e0875b2ceb8a08f23b557a7fd" https://api.github.com/search/commits\?q\=author:eFuller96
+//curl -H "Accept: application/vnd.github.cloak-preview" -H "Authorization: token adbde95cd6986e8e0875b2ceb8a08f23b557a7fd" https://api.github.com/search/commits\?q\=author:eFuller96+repo:red-gate/foundry-labs+author-date:<2019-12-31
